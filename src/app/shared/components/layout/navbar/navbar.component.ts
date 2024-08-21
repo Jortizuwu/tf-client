ca00383fc8c062e668829b30368c0551ccb42994
Jortizuwu <joseher2511@gmail.com>
2024-08-21 11:52:45 -0500

    update navbar

--- src/app/shared/components/layout/navbar/navbar.component.ts
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { LinkComponent } from '../../atoms/link/link.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-layout-navbar',
   standalone: true,
-  imports: [CommonModule, LinkComponent],
+  imports: [CommonModule, LinkComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
